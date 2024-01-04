13f13607c1a8f4ba0b2988ab080c4c2ea2120073
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-01-04 10:40:20 +0100

    added grpc client

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use payments::bitcoin_client::BitcoinClient;
+use payments::BtcPaymentRequest;
+
+pub mod payments {
+    tonic::include_proto!("payments");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = BitcoinClient::connect("http://[::1]:5051").await?;
+
+    let request = tonic::Request::new(BtcPaymentRequest {
+        from_addr: "1234".to_owned(),
+        to_addr: "23456".to_owned(),
+        amount: 209
+    });
+
+    let response = client.send_payment(request).await?;
+
+    println!("Response {:?}", response);
+    Ok(())
+}
